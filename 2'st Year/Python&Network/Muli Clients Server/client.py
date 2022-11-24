eaa009deac91f0a38f9c128592351b9463928c82
Roei Barak <roei.barak@intel.com>
2022-11-24 16:28:36 +0200

    working on client

--- 2'st Year/Python&Network/Muli Clients Server/client.py
+++ 2'st Year/Python&Network/Muli Clients Server/client.py
@@ -1,25 +1,32 @@
 import socket
 import msvcrt
+import protocol
+import select
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.connect(("127.0.0.1", 5555))
 msg = input("Pls enter msg\n")
+client_sockets = []
 
 while msg != "EXIT":
-
+    rlist, w_list, xlist = select.select([my_socket], client_sockets, [], 0.5)
+    print("here1")
     if msvcrt.kbhit():
         while True:
             ch = msvcrt.getch().decode()
-            print(ch)
+            # print(ch)
             if ch == '\r':
                 print("ASd")
                 break
             msg = msg + ch
-            print(msg)
-            print(ch, end="", flush=True)
+            print(ch, end= "", flush=True)
     print("spme")
-    my_socket.send(msg.encode())
-    data = my_socket.recv(1024).decode()
-    print("Server: ", data)
+    my_socket.send(protocol.create_msg(msg).encode())
+    msg = ""
+    for current_socket in rlist:
+        print("here socket")
+        data = current_socket.recv(1024).decode()
+        print("Server: ", data)
+
 
 my_socket.close()
