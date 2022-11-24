a595aa82e32af0c6fd97b915bc4cb07fc21522f3
Roei Barak <roei.barak@intel.com>
2022-11-24 16:07:13 +0200

    working on client

--- 2'st Year/Python&Network/Muli Clients Server/client.py
+++ 2'st Year/Python&Network/Muli Clients Server/client.py
@@ -1,23 +1,25 @@
 import socket
-import sys
-
-import protocol
 import msvcrt
 
-flush = False
-ch3 = ''
-while not msvcrt.kbhit() and ch3 != '\r':
-    ch3 = msvcrt.getwche()
-
-SERVER_PORT = 5555
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(("127.0.0.1", SERVER_PORT))
-msg = msvcrt.getwch()
+my_socket.connect(("127.0.0.1", 5555))
+msg = input("Pls enter msg\n")
 
 while msg != "EXIT":
+
+    if msvcrt.kbhit():
+        while True:
+            ch = msvcrt.getch().decode()
+            print(ch)
+            if ch == '\r':
+                print("ASd")
+                break
+            msg = msg + ch
+            print(msg)
+            print(ch, end="", flush=True)
+    print("spme")
     my_socket.send(msg.encode())
     data = my_socket.recv(1024).decode()
-    print("server replied:", data)
-    msg = input("Pls enter message\n")
+    print("Server: ", data)
 
 my_socket.close()
