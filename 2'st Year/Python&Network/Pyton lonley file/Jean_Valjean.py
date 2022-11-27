b90155f1a1545f5efa02833d8cdc1cbe05ee4ee0
roeib <roeibarak123@gmail.com>
2022-11-27 19:31:47 +0200

    working on the server

--- 2'st Year/Python&Network/Pyton lonley file/Jean_Valjean.py
+++ 2'st Year/Python&Network/Pyton lonley file/Jean_Valjean.py
@@ -1,32 +1,20 @@
-# """
-# Roei Barak
-# 318419611
-# """
-# LEN_OF_STRING = 5
-#
-#
-# def main():
-#     num = input("Please enter 5 digit number\n")
-#     print(f"You entered the number:{num}")
-#     print(f"The digit of this number are:{num[0]},{num[1]},{num[2]},{num[3]},{num[4]}")
-#     sum_dig = 0
-#     for i in range(LEN_OF_STRING):
-#         sum_dig += int(num[i])
-#     print(f"The sum of digits is:{sum_dig}")
-#
-#
-# # Standard boilerplate to call the main() function.
-# if __name__ == '__main__':
-#     main()
-cmd = "MSGat est"
-DIC = {'a' : 1,'b': 2 }
-print(' '.join(DIC.keys()))
-cmd_parsed = cmd.split(' ')
-cmd_name = cmd_parsed[0]
-recipient_name = cmd_parsed[1]
-msg = ' '.join(cmd_parsed[2:])
+"""
+Roei Barak
+318419611
+"""
+LEN_OF_STRING = 5
 
-print(cmd_name)
-print(recipient_name)
 
-print(msg)
+def main():
+    num = input("Please enter 5 digit number\n")
+    print(f"You entered the number:{num}")
+    print(f"The digit of this number are:{num[0]},{num[1]},{num[2]},{num[3]},{num[4]}")
+    sum_dig = 0
+    for i in range(LEN_OF_STRING):
+        sum_dig += int(num[i])
+    print(f"The sum of digits is:{sum_dig}")
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+    main()
