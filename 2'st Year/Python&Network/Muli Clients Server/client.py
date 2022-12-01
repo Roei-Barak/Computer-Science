210120b7b3dd8202a88fd3bd5ab94123bb5a00be
Roei Barak <roei.barak@intel.com>
2022-12-01 14:47:30 +0200

    working on Server

--- 2'st Year/Python&Network/Muli Clients Server/client.py
+++ 2'st Year/Python&Network/Muli Clients Server/client.py
@@ -8,10 +8,9 @@ my_socket.connect(("127.0.0.1", 5555))
 user_input = input("Pls enter msg\n")
 client_sockets = []
 while user_input != "EXIT":
-
+    print(user_input)
     rlist, w_list, xlist = select.select([my_socket], client_sockets, [], 0.5)
     if msvcrt.kbhit():
-        print("1")
         while True:
             ch = msvcrt.getch().decode()
             # print(ch)
@@ -21,7 +20,11 @@ while user_input != "EXIT":
             user_input = user_input + ch
             print(ch, end="", flush=True)
     if user_input != '':
-
+        print("1")
+        if user_input == 'EXIT':
+            print('4')
+            my_socket.close()
+            break
         # 1. Add length field ("HELLO" -> "04HELLO")
         message = protocol.create_msg(user_input)
         # 2. Send it to the server
@@ -33,14 +36,17 @@ while user_input != "EXIT":
         # user_input = ""
         # print('3')
     for current_socket in rlist:
-        print('4')
 
         valid_msg, server_resp = protocol.get_msg(my_socket)
+        if server_resp == 'EXIT':
+            my_socket.close()
+            break
         print('6')
         if valid_msg:
             print("Server sent: ",  server_resp)
         else:
             print("Wrong protocol")
     user_input = ''
+
 
 my_socket.close()
