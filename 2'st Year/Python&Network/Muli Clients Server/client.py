85a0407e274e1258c8c461416c755579f6ee17a4
roeib <roeibarak123@gmail.com>
2022-12-01 19:39:04 +0200

    working on the server

--- 2'st Year/Python&Network/Muli Clients Server/client.py
+++ 2'st Year/Python&Network/Muli Clients Server/client.py
@@ -5,42 +5,25 @@ import select
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.connect(("127.0.0.1", 5555))
-user_input = input("Pls enter msg\n")
+msg = input("Pls enter msg\n")
 client_sockets = []
-while user_input != "EXIT":
 
+while msg != "EXIT":
     rlist, w_list, xlist = select.select([my_socket], client_sockets, [], 0.5)
     if msvcrt.kbhit():
-        print("1")
         while True:
             ch = msvcrt.getch().decode()
             # print(ch)
             if ch == '\r':
-                print("Enter has been pressed")
+                print("ASd")
                 break
-            user_input = user_input + ch
-            print(ch, end="", flush=True)
-    if user_input != '':
-
-        # 1. Add length field ("HELLO" -> "04HELLO")
-        message = protocol.create_msg(user_input)
-        # 2. Send it to the server
-        my_socket.send(message.encode())
-        # 3. Get server's response
-        # if user_input == "":
-        #     print('4')
-        #     continue
-        # user_input = ""
-        # print('3')
+            msg = msg + ch
+            print(ch, end= "", flush=True)
+    my_socket.send(protocol.create_msg(msg).encode())
+    msg = ""
     for current_socket in rlist:
-        print('4')
+        data = protocol.get_msg(current_socket)
+        print("Server sent: ", data)
 
-        valid_msg, server_resp = protocol.get_msg(my_socket)
-        print('6')
-        if valid_msg:
-            print("Server sent: ",  server_resp)
-        else:
-            print("Wrong protocol")
-    user_input = ''
 
 my_socket.close()
