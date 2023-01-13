d466400e9fd25018a9bfd4561c98a9d789de17a3
roeib <roeibarak123@gmail.com>
2023-01-13 15:56:50 +0200

    Revert "working on the server"
    This reverts commit 34687d13

--- 2'st Year/Python&Network/SYN_FLOOD/SYN_Flood.py
+++ 2'st Year/Python&Network/SYN_FLOOD/SYN_Flood.py
@@ -1,2 +1,22 @@
+from scapy.all import *
+from scapy.layers.inet import IP, TCP
 
-pcapFile = rdpcap("SynFloodSample.pcap")
+IP_list = []
+
+
+def check(pkt):
+    if pkt.haslayer(TCP):
+        if pkt[IP][TCP].flags == 'S' and pkt[IP].src[]:
+            return True
+
+
+pcapFile = rdpcap("SYNflood.pcapng")
+
+# tcp.flags.syn ==1 and tcp.flags.ack ==0
+
+for pkt in pcapFile:
+
+    if check(pkt):
+        with open("temp.txt", "w") as f:
+            f.write(pkt[IP].src)
+        print(pkt.time)
